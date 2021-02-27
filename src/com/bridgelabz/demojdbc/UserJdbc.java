1d60450ff5ccd8bcbd5b2d77a495073f9a1db429
ManaliParab12 <manaliparab10@gmail.com>
2021-02-27 20:52:21 +0530

    Inserted data using prepared statement into table

--- src/com/bridgelabz/demojdbc/UserJdbc.java
+++ src/com/bridgelabz/demojdbc/UserJdbc.java
@@ -2,29 +2,38 @@ package com.bridgelabz.demojdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class UserJdbc {
 
-	private static final String DELETE_USER_FROM_TABLE = "DELETE FROM user  WHERE id = 2;"; 
+	private static final String INSERT_USER_INTO_TABLE = "INSERT INTO user"
+	                    + " (id, name, email, address, password) VALUES " 
+			            + " (?, ?, ?, ?, ?);"; 
 
 	public static void main(String[] args) throws SQLException {
 		UserJdbc userJdbc = new UserJdbc();
-		userJdbc.deleteRecord();
+		userJdbc.insertRecord();
 	}
 
-	public void deleteRecord() throws SQLException {
-		
-		System.out.println(DELETE_USER_FROM_TABLE);
-
+	public void insertRecord() throws SQLException {		
+		System.out.println(INSERT_USER_INTO_TABLE);
 		try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_demo?useSSL=false",
 				"root", "Manali@123");
 
-				Statement statement = connection.createStatement();) {
+				PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USER_INTO_TABLE)) {
+			//setting placeholder values
+			preparedStatement.setInt(1, 2);
+			preparedStatement.setString(2, "Tony");
+			preparedStatement.setString(3, "tony@gmail.com");
+			preparedStatement.setString(4, "Abc");
+			preparedStatement.setString(5, "secret");
+			
+			System.out.println(preparedStatement);
 
-			int result = statement.executeUpdate(DELETE_USER_FROM_TABLE);
+			int result = preparedStatement.executeUpdate();
 			System.out.println("No. of records affected: " +result);
 		} catch (SQLException e) {
 	
